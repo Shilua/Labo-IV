92a3f046f66f29bafe88e81cde7c935c21611572
BuildTools <unconfigured@null.spigotmc.org>
2020-09-03 22:15:52 -0300

    user in localStorage

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -12,12 +12,17 @@ export class LoginComponent implements OnInit {
   public user:String;
   public password:String
   public userCreds: User;
+  public usuarioString:string;
 
   constructor(private router: Router) {}
 
   login(){
+    this.userCreds.user = this.user;
+    this.userCreds.password = this.password;
     if (this.user == 'admin' && this.password == 'admin') {
       this.router.navigate(['/Bienvenido']);
+      this.usuarioString = JSON.stringify(this.userCreds);
+      localStorage.setItem('usuario', this.usuarioString);
     } else {
       this.router.navigate(['/Error']);
     }
