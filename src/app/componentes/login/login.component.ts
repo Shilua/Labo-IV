02e10ea94ed15bc97832d6b01b0e826f4571d1f2
BuildTools <unconfigured@null.spigotmc.org>
2020-09-03 22:23:48 -0300

    fix for login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -14,13 +14,17 @@ export class LoginComponent implements OnInit {
   public userCreds: User;
   public usuarioString:string;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    //this.userCreds = new User();
+  }
 
   login(){
-    this.userCreds.user = this.user;
-    this.userCreds.password = this.password;
+    
     if (this.user == 'admin' && this.password == 'admin') {
+      //this.userCreds.user = this.user;
+      //this.userCreds.password = this.password;
       this.router.navigate(['/Bienvenido']);
+
       this.usuarioString = JSON.stringify(this.userCreds);
       localStorage.setItem('usuario', this.usuarioString);
     } else {
